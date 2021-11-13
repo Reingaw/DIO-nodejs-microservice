624984cfae16a62abb905b7873fc41b84d1259f4
Wagner Andrade <wagnerlinhares@bol.com.br>
2021-11-13 02:38:18 -0300

    Set routes and add http-status-codes library

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import usersRoute from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(usersRoute);
 
 app.listen(3000,()=> {
     console.log('Server running at http://localhost:3000');
